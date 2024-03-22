bbf30a754e3b2377422109e83875cac6eafa9dff
totiruocco0 <162723302+totiruocco0@users.noreply.github.com>
2024-03-22 12:30:38 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
-def ada():
+  def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    fullname = first_name + " " + last_name
+    print(fullname.lower())
+    print(fullname.upper())
+    fullnamelower = fullname.lower()
+    print(fullnamelower.title())
+ada()
